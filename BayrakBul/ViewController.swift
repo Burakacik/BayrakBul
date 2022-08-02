488abc43f6319263faa934f101287287acab53b0
Burakacik <burakacik@outlook.com>
2022-08-02 20:42:55 +0300

    Initial Commit

---
+++ BayrakBul/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BayrakBul
+//
+//  Created by Burak AÇIK on 2.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
