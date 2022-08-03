035f49169b4c46d6e7dded800bdefc64a3c5f55f
Burakacik <burakacik@outlook.com>
2022-08-03 13:38:07 +0300

    VC Designed
    QuizVC added
    ResultVC added
    VC designed

--- BayrakBul/ViewController.swift
+++ BayrakBul/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playerNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
     }
 
-
+    @IBAction func startQuizClicked(_ sender: Any) {
+    }
+    
 }
 
